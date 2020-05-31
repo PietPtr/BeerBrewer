e6c76836fd92c3c699762c5c3f08b6e8c5c34c74
Pieter Staal <pieterstaal@outlook.com>
2020-05-31 12:01:49 +0200

    Add PID control strat

--- maischen.py
+++ maischen.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import os, time
 from temperature import fetch_temp
+from simple_pid import PID
 
 TIME_STEP_SIZE = 2
 RELAY_PORT = 14
@@ -14,18 +15,26 @@ def on():
 def off():
     GPIO.output(RELAY_PORT, 1)
 
+def log(diff, measured_temp, setpoint, value=""):
+    print(str(int(diff // 60)) + "m " + str(int(diff) % 60) + "s: measured(", measured_temp, ") setpoint(", setpoint, ")", value)
+
+def get_setpoint(profile, diff):
+    current_minute = diff // 60
+    setpoint = 0
+
+    for (minute, phase_temp) in profile:
+            if current_minute < minute:
+                setpoint = phase_temp
+                break
+
+    return setpoint
+
 def maisch(profile, margin):
     start_time = time.time()
     while True:
         now = time.time()
         diff = now - start_time
-        current_minute = diff // 60
-        setpoint = 0
-
-        for (minute, phase_temp) in profile:
-            if current_minute < minute:
-                setpoint = phase_temp
-                break
+        setpoint = get_setpoint(profile, diff)
 
         measured_temp = fetch_temp()
 
@@ -34,23 +43,58 @@ def maisch(profile, margin):
         elif measured_temp - margin <= setpoint:
             on()
 
-        print(str(int(current_minute)) + "m " + str(int(diff) % 60) + "s: measured(", measured_temp, ") setpoint(", setpoint, ")")
+        #print(str(int(current_minute)) + "m " + str(int(diff) % 60) + "s: measured(", measured_temp, ") setpoint(", setpoint, ")")
+        log(diff, measured_temp, setpoint)
 
         time.sleep(TIME_STEP_SIZE)
 
 
+def maisch_pid(profile, INTERVAL):
+    pid = PID(10, 0, 0, setpoint=0)
+
+    start_time = time.time()
+    while True:
+        now = time.time()
+        diff = now - start_time
+        pid.setpoint = get_setpoint(profile, diff)
+
+        temp = fetch_temp()
+        value = pid(temp)
+        value = min(value, 100)
+        prop = value / 100.0
+
+
+        log(diff, temp, pid.setpoint, value=value)
+
+        if value <= 0:
+            off()
+            time.sleep(INTERVAL)
+        elif value == 100:
+            on()
+            time.sleep(INTERVAL)
+        else:
+            on()
+            time.sleep(prop * INTERVAL)
+            off()
+            time.sleep((1 - prop) * INTERVAL)
+
+
+
+
 if __name__ == '__main__':
-    profiel = [
-        (5, 58),
-        (50, 62),
-        (53, 68),
-        (60, 72)
+    maischen = [
+        (45, 63),
+        (65, 68),
+        (95, 73),
+        (100, 78)
     ]
+
+    pidtest = [(50000, 60)]
 
     margin = 2.5
 
     try:
-        maisch(profiel, margin)
+        maisch_pid(pidtest, 10)
     except BaseException as e:
         print(e)
         off()
